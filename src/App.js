9dc288a064e3bf41fa900f5256f385687f0b1e04
pingw206 <pingw206@gmail.com>
2020-06-12 15:51:52 +0800

    前端部分完成

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Signin from './components/Signin/Signin';
+import Register from './components/Register/Register';
 import Particles from 'react-particles-js';
 import Clarifai from 'clarifai';
+import './App.css';
 
 const app = new Clarifai.App({
   apiKey: 'ba971b2e130a47ba8e945978949b6db5'
@@ -26,39 +28,74 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      input: ''
+      input: '',
+      imageUrl:'',
+      box:{},
+      route: 'signin',
+      isSignedIn: false
+    }
+  }
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
     }
   }
 
+  displayFaceBox = (box) => {
+    this.setState({box:box});
+  }
   onInputChange = (event) => {
-    console.log(event.target.value);
+    this.setState({input:event.target.value});
   }
   onButtonSubmit = () => {
-    console.log('click');
-    app.models.predict("a403429f2ddf4b49b307e318f00e528b", "https://samples.clarifai.com/face-det.jpg").then(
-      function(response) {
-        console.log(response);
-      },
-      function(err) {
-      // there was an error
-      }
-    );
+    this.setState({imageUrl: this.state.input});
+    app.models
+      .predict(
+        Clarifai.FACE_DETECT_MODEL, 
+        this.state.input)
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .catch(err => console.log(err));
+  }
+  onRouteChange = (route) => {
+    if (route === 'signout') {
+      this.setState({isSignedIn: false})
+    } else if (route === 'home') {
+      this.setState({isSignedIn: true})
+    }
+    this.setState({route: route});
   }
   render() {
+    const { isSignedIn, imageUrl, route, box } = this.state;
     return (
       <div className="App">
         <Particles 
           className="particles"
           params={particlesOptions}
         />
-        <Navigation />
-        <Logo />
-        <Rank />
-        <ImageLinkForm 
-          onInputChange={this.onInputChange}
-          onButtonSubmit={this.onButtonSubmit}
-        />
-        <FaceRecognition />
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange}/>
+        { route === 'home'
+          ? <div>
+              <Logo />
+              <Rank />
+              <ImageLinkForm 
+                onInputChange={this.onInputChange}
+                onButtonSubmit={this.onButtonSubmit}
+              />
+              <FaceRecognition box={box} imageUrl={imageUrl}/>
+            </div>
+          : (
+            route === 'signin'
+            ? <Signin onRouteChange={this.onRouteChange} />
+            : <Register onRouteChange={this.onRouteChange} />
+          )
+        } 
     </div>
   );
 }
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,13 +1,26 @@
 import React from 'react';
 
-const Navigation = () => {
-  return (
-    <nav style={{display: 'flex', justifyContent:'flex-end'}}>
-      <p className='f3 link dim black underline pa3 pointer'>
-        Sign out
-      </p>
+const Navigation = ({onRouteChange, isSignedIn}) => {
+  if (isSignedIn) {
+    return (
+      <nav style={{display: 'flex', justifyContent:'flex-end'}}>
+        <p onClick={() => onRouteChange('signout')} className='f3 link dim black underline pa3 pointer'>
+          退出
+        </p>
     </nav>
-  )
+    )
+  } else {
+    return (
+      <nav style={{display: 'flex', justifyContent:'flex-end'}}>
+        <p onClick={() => onRouteChange('signin')} className='f3 link dim black underline pa3 pointer'>
+          登录
+        </p>
+        <p onClick={() => onRouteChange('register')} className='f3 link dim black underline pa3 pointer'>
+          注册
+        </p>
+    </nav>
+    )
+  }
 }
 
 export default Navigation;
